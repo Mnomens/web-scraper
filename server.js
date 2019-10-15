12cc7984f983ae4ae5fba6b3bd7af40b7fe8c3c2
Mnomens <48028258+Mnomens@users.noreply.github.com>
2019-10-15 15:26:13 -0400

    mongoose uri added

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 const routes = require('./routes/routes.js');
 app.use(routes);
